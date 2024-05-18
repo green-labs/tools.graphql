c9b25c5f34e4900696adb380bad15dc9b6c436e3
Hyunwoo Nam <namenu@gmail.com>
2024-05-19 02:14:56 +0900

    validator-test 수정

--- src/tools/graphql/validators.clj
+++ src/tools/graphql/validators.clj
@@ -1,30 +1,26 @@
 (ns tools.graphql.validators
   (:require [meander.epsilon :as m]))
 
-(defn- graphql-type?
-  "
-  <type> ::= <named_type>
-           | <list_type>
-           | <non_null_type>
+(defn graphql-type?
+  "Can we match :User with (list (non-null User)) ?
 
-  <list_type> ::= \"[\" <type> \"]\"
-
-  <non_null_type> ::= <type> \"!\"
-
-  <named_type> ::= <type_name>
-
-  <type_name> ::= <name>
-
-  <name> ::= [_A-Za-z][_0-9A-Za-z]*
-  "
+  formal grammar:
+    <type> ::= <named_type>
+             | <list_type>
+             | <non_null_type>
+    <list_type> ::= \"[\" <type> \"]\"
+    <non_null_type> ::= <type> \"!\"
+    <named_type> ::= <type_name>
+    <type_name> ::= <name>
+    <name> ::= [_A-Za-z][_0-9A-Za-z]*"
   [t]
-  (m/find t
-          (m/with [%list-type     (list %type)
-                   %non-null-type (non-null %type)
-                   %named-type    (m/keyword !type-name)
-                   %type          (m/or %named-type %list-type %non-null-type)]
-            %type)
-          !type-name))
+  (some? (m/find t
+                 (m/with [%list-type     (list %type)
+                          %non-null-type (non-null %type)
+                          %named-type    (m/keyword !type-name)
+                          %type          (m/or %named-type %list-type %non-null-type)]
+                         %type)
+                 !type-name)))
 
 (defn unreachable-types [schema]
   (let [types        (m/search schema
@@ -33,13 +29,23 @@
         unreachable? (fn [t]
                        (nil? (m/find {:schema schema
                                       :type   t}
+                                     ;; objects
                                      {:schema {:objects {?type {:fields {?field {:type (m/$ ?t)}}}}}
                                       :type   ?t}
                                      ?field
-                                     ;; TODO: input-objects, enums, interfaces, unions
+
+                                     ;; queries and mutations
                                      {:schema {(m/or :queries :mutations) {?qm {:type (m/$ ?t)}}}
                                       :type   ?t}
-                                     ?qm)))]
+                                     ?qm
+
+                                     ;; unions
+                                     {:schema {:unions {?union {:members (m/$ ?t)}}}
+                                      :type   ?t}
+                                     ?union
+
+                                     ;; TODO: input-objects, enums, interfaces
+                                     )))]
     (println "types:" (sort types))
     (filter unreachable? types)))
 
@@ -69,13 +75,4 @@
   (unreachable-types schema)
   (unreachable-input-types schema)
 
-  ;; can we match :User with (list (non-null User)) ?
-
-  (let [ts [:User
-            '(list :User)
-            '(non-null :User)
-            '(list (non-null :User))
-            '(non-null (list :User))
-            '(non-null (list (list :User)))
-            '(:User list)]]
-    (map graphql-type? ts)))
+  :rcf)
--- test/tools/graphql/validators_test.clj
+++ test/tools/graphql/validators_test.clj
@@ -2,35 +2,42 @@
   (:require [clojure.test :refer :all]
             [tools.graphql.validators :as v]))
 
-(deftest no-unreachable-types
-  (let [schema {:objects   {:User  {:fields {:id        {:type 'String}
-                                             :name      {:type 'String}
-                                             :email     {:type 'String}
-                                             :password  {:type 'String}
-                                             :createdAt {:type 'String}
-                                             :updatedAt {:type 'String}
-                                             :deletedAt {:type :Date}}}
-                            :Date  {:fields {:year  {:type 'Int}
-                                             :month {:type 'Int}
-                                             :day   {:type 'Int}}}
-                            :Dummy {:fields {:id {:type 'String}}}}
-                :queries   {:user     {:type :User}
-                            :users    {:type '(non-null (list (non-null :User)))}
-                            :post     {:type :Post}
-                            :posts    {:type '(non-null (list (non-null :Post)))}
-                            :comment  {:type :Comment}
-                            :comments {:type '(non-null (list (non-null :Comment)))}}
-                :mutations {:createUser    {:type :User}
-                            :updateUser    {:type :User}
-                            :deleteUser    {:type :User}
-                            :createPost    {:type :Post}
-                            :updatePost    {:type :Post}
-                            :deletePost    {:type :Post}
-                            :createComment {:type :Comment}
-                            :updateComment {:type :Comment}
-                            :deleteComment {:type :Comment}}}]
-    (is (= [:Dummy] (v/unreachable-types schema)))))
+(deftest syntax-test
+  (testing "type in nested type"
+    (let [ts [:User
+              '(list :User)
+              '(non-null :User)
+              '(list (non-null :User))
+              '(non-null (list :User))
+              '(non-null (list (list :User)))
+              '(:User list)]]
+      (is (= [true true true true true true false]
+             (map v/graphql-type? ts))))))
 
+(deftest no-unreachable-types
+  (testing "object not referenced in any queries or mutations"
+    (let [schema {:objects   {:User  {:fields {:id        {:type 'String}
+                                               :name      {:type 'String}
+                                               :email     {:type 'String}
+                                               :password  {:type 'String}
+                                               :createdAt {:type 'String}
+                                               :updatedAt {:type 'String}
+                                               :deletedAt {:type :Date}}}
+                              :Date  {:fields {:year  {:type 'Int}
+                                               :month {:type 'Int}
+                                               :day   {:type 'Int}}}
+                              :Dummy {:fields {:id {:type 'String}}}}
+                  :queries   {:user     {:type :User}
+                              :users    {:type '(non-null (list (non-null :User)))}}
+                  :mutations {:createUser {:type :User}
+                              :updateUser {:type :User}
+                              :deleteUser {:type :User}}}]
+      (is (= [:Dummy] (v/unreachable-types schema)))))
+
+  (testing "object referenced in union"
+    (let [schema {:objects {:UnionMember {:fields {:id {:type 'String}}}}
+                  :unions  {:TheUnion {:members [:Pre1 :Pre2 :UnionMember :Post1 :Post2]}}}]
+      (is (= [] (v/unreachable-types schema))))))
 
 (deftest no-unreachable-input-types
   (testing "input-object not referenced in any query or mutation"
